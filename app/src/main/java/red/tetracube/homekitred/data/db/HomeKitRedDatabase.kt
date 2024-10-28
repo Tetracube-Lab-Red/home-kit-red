b48578fb5756327db97ed19c9377a8b2dfc73629
Davies <davide.dcube@gmail.com>
2024-10-29 00:08:50 +0100

    🏗 Add devices entity to handle devices

--- app/src/main/java/red/tetracube/homekitred/data/db/HomeKitRedDatabase.kt
+++ app/src/main/java/red/tetracube/homekitred/data/db/HomeKitRedDatabase.kt
@@ -1,26 +1,32 @@
 package red.tetracube.homekitred.data.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import red.tetracube.homekitred.data.db.datasource.DeviceDataSource
 import red.tetracube.homekitred.data.db.datasource.HubDatasource
 import red.tetracube.homekitred.data.db.datasource.RoomDatasource
+import red.tetracube.homekitred.data.db.entities.DeviceEntity
 import red.tetracube.homekitred.data.db.entities.HubEntity
 import red.tetracube.homekitred.data.db.entities.RoomEntity
 
 @Database(
     entities = [
         HubEntity::class,
-        RoomEntity::class
+        RoomEntity::class,
+        DeviceEntity::class
     ],
-    version = 1,
+    version = 2,
     autoMigrations = [
+        AutoMigration(from = 1, to = 2)
     ]
 )
 abstract class HomeKitRedDatabase : RoomDatabase() {
     abstract fun hubRepository(): HubDatasource
     abstract fun roomRepository(): RoomDatasource
+    abstract fun deviceRepository(): DeviceDataSource
 
     companion object {
         private const val DATABASE_FILE_NAME = "home_kit_red.db"
